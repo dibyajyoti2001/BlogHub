21c6a5f11535cc6d21ea4951eb6710e5ea9e5eeb
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-08-24 12:38:57 +0530

    update and fix the bug on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -37,25 +37,31 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="px-8 md:px-[200px] min-h-[80vh]">
-        {loader ? (
-          <div className="h-[40vh] flex justify-center items-center">
-            <Loader />
+      {user && (
+        <>
+          <div className="px-8 md:px-[200px] min-h-[80vh]">
+            {loader ? (
+              <div className="h-[40vh] flex justify-center items-center">
+                <Loader />
+              </div>
+            ) : !noResults ? (
+              posts.map((post) => (
+                <Link
+                  key={post._id}
+                  to={user ? `/posts/post/${post._id}` : "/login"}
+                >
+                  <HomePosts post={post} />
+                </Link>
+              ))
+            ) : (
+              <h3 className="text-center font-bold mt-16">
+                No posts available
+              </h3>
+            )}
           </div>
-        ) : !noResults ? (
-          posts.map((post) => (
-            <Link
-              key={post._id}
-              to={user ? `/posts/post/${post._id}` : "/login"}
-            >
-              <HomePosts post={post} />
-            </Link>
-          ))
-        ) : (
-          <h3 className="text-center font-bold mt-16">No posts available</h3>
-        )}
-      </div>
-      <div className="mt-8"></div>
+          <div className="mt-8"></div>
+        </>
+      )}
     </>
   );
 }
