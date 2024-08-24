a2176c724438c1e57163397d65ba4c7157cc55b8
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-08-24 12:49:04 +0530

    fix bug on login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,7 +28,6 @@ export default function Login() {
       navigate("/");
     } catch (error) {
       setError(true);
-      console.error("Login error:", error);
       alert("Login error: " + error.message);
     }
   };
