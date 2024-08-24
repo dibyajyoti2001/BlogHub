f8911835e9eec12d2d739b3f891e6104758d71f7
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-08-24 12:33:06 +0530

    update and fix the bugs on user context

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,60 +1,37 @@
 import { createContext, useEffect, useState } from "react";
-import { refetchUser } from "../server/api";
-// import { LocalStorage } from "../utils";
+import { refetchUser, refreshUser } from "../server/api";
+import { LocalStorage } from "../utils";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
 
-  // const getUser = async () => {
-  //   const token = LocalStorage.get("accessToken");
-  //   console.log("Token: " + token);
-
-  //   if (!token) {
-  //     alert("Token not found");
-  //     return;
-  //   }
-
-  //   try {
-  //     const res = await refetchUser();
-  //     console.log("User refetched successfully:", res.data.data);
-  //     setUser(res.data.data);
-  //   } catch (err) {
-  //     console.error("Error refetching user:", err.response || err.message);
-
-  //     if (err.response?.status === 401) {
-  //       try {
-  //         console.log("Attempting to refresh token...");
-  //         await refreshUser();
-  //         const res = await refetchUser();
-  //         console.log(
-  //           "User refetched successfully after refresh:",
-  //           res.data.data
-  //         );
-  //         setUser(res.data.data);
-  //       } catch (err) {
-  //         console.error(
-  //           "Error after token refresh:",
-  //           err.response || err.message
-  //         );
-  //         setUser(null);
-  //         alert(err.message);
-  //       }
-  //     } else {
-  //       setUser(null);
-  //       alert(err.message);
-  //     }
-  //   }
-  // };
-
   const getUser = async () => {
+    const token = LocalStorage.get("accessToken");
+
+    if (!token) {
+      alert("Token not found");
+      return;
+    }
+
     try {
       const res = await refetchUser();
       setUser(res.data.data);
     } catch (error) {
-      alert(error.message);
-      setUser(null);
+      if (error.response?.status === 401) {
+        try {
+          await refreshUser();
+          const res = await refetchUser();
+          setUser(res.data.data);
+        } catch (error) {
+          setUser(null);
+          alert(error.message);
+        }
+      } else {
+        setUser(null);
+        alert(error.message);
+      }
     }
   };
 
